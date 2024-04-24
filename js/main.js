61990bbffe266b9272c78382d91d9fa48dba139a
PrestigiousPreston <prestonmaina75@gmail.com>
2024-04-24 23:01:56 +0300

    Modified header, color and events section
    Removed sticky top property from header.
    Used MUCU brand colors.
    Added content on the events section.

--- js/main.js
+++ js/main.js
@@ -12,3 +12,108 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
+// Javascript for header Scrollspy
+
+// get the header
+// var header = document.getElementById("header");
+// var lastScrollTop = 0;
+
+// // on scroll down, hide the header; on scroll up, show the header
+// window.addEventListener("scroll", function(){
+//     var currentScroll = window.scrollY || this.document.documentElement.scrollTop;
+
+//     if (currentScroll > lastScrollTop){
+//         // downscroll code
+//         header.style.top = "-100px"; //change this value as needed
+//     } else {
+//         // upscroll code
+//         header.style.top = 0;
+//     }
+
+//     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // for mobile or negative scrolling
+// }, false);
+
+
+
+// Javascript for tab navigation horizontal scroll buttons
+const btnLeft = document .querySelector(".left-btn");
+const btnRight = document .querySelector(".right-btn");
+const tabMenu = document .querySelector(".tab-menu");
+
+const IconVisibility = () => {
+    let scrollLeftValue = Math.ceil(tabMenu.scrollLeft);
+    // console.log("1.", scrollLeftValue);
+    let scrollableWidth = tabMenu.scrollWidth - tabMenu.clientWidth;
+    // console.log("2.", scrollableWidth);
+
+    btnLeft.style.display = scrollLeftValue > 0 ? "block" : "none";
+    btnRight.style.display = scrollableWidth > scrollLeftValue ? "block" : "none";
+}
+
+btnRight.addEventListener("click", () => {
+    tabMenu.scrollLeft += 102;
+    // IconVisibility();
+    setTimeout(() => IconVisibility(), 50);
+});
+
+btnLeft.addEventListener("click", () => {
+    tabMenu.scrollLeft -= 102;
+    // IconVisibility();
+    setTimeout(() => IconVisibility(), 50);
+});
+
+window.onload = function(){
+    btnRight.style.display = tabMenu.scrollWidth > tabMenu.clientWidth || tabMenu.scrollWidth >= window.innerWidth ? "block" : "none";
+    btnLeft.style.display = tabMenu.scrollwidth >= window.innerWidth ? "" : "none";
+}
+
+window.onresize = function(){
+    btnRight.style.display = tabMenu.scrollWidth > tabMenu.clientWidth || tabMenu.scrollWidth >= window.innerWidth ? "block" : "none";
+    btnLeft.style.display = tabMenu.scrollwidth >= window.innerWidth ? "" : "none";
+
+    let scrollLeftValue = Math.round(tabMenu.scrollLeft);
+
+    btnLeft.style.display = scrollLeftValue > 0 ? "block" : "none";
+}
+
+// Javascript to make the navigation draggable
+let activeDrag = false;
+
+tabMenu.addEventListener("mousemove", (drag) => {
+    if(!activeDrag) return;
+    tabMenu.scrollLeft -= DragEvent.movementX;
+    IconVisibility();
+    tabMenu.classList.add("dragging");
+});
+
+document.addEventListener("mouseup", () => {
+    activeDrag = false;
+    tabMenu.classList.remove("dragging");
+});
+
+tabMenu.addEventListener("mousedown", () => {
+    activeDrag = true;
+});
+
+// Javascript to view tab contents on click tab buttons
+const tabs = document.querySelectorAll(".tab");
+const tabBtns = document.querySelectorAll(".tab-btn");
+
+const tab_Nav = function(tabBtnClick){
+    tabBtns.forEach((tabBtn) => {
+        tabBtn.classList.remove("active");
+    });
+    tabs.forEach((tab) => {
+        tab.classList.remove("active");
+    });
+
+    tabBtns[tabBtnClick].classList.add("active");
+    tabs[tabBtnClick].classList.add("active");
+}
+
+tabBtns.forEach((tabBtn, i) => {
+    tabBtn.addEventListener("click", () =>{
+        tab_Nav(i);
+    });
+});

725bffb443d000f3c55dc70c70b08c122e6fdc0a
PrestigiousPreston <prestonmaina75@gmail.com>
2024-07-24 03:56:59 +0300

    fixed broken links

--- public/scripts/script.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu(){
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".humberger-icon");
-  
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
